8e80d7e281e7af41b888b62b425bf8e4dc7d56eb
Tomoko <okomot1t@yahoo.com>
2022-11-23 13:37:27 -0600

    adding updated files up to 12-5-3

---
+++ static/js/app.js
@@ -0,0 +1,49 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Simple JavaScript console.log statement
+function printHello();{
+    console.log("Hello there!");
+  }
+// Takes two numbers and adds them
+function addition(a, b) {
+    return a + b;
+  }
+// Converted to an arrow function
+addition = (a, b) => a + b;
+// Converted to an arrow function
+doubleAddition = (c, d) => addition(c, d) * 2;
+
+/// Create a table 
+function buildTable(data) {
+  // First, clear out any existing data
+  tbody.html("");
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+    
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );  
+});
+}
+/// Note from Module 11-5-2
+//Add the forEach Function
+// - Looped through each object in the array
+// - Appended a row to the HTML table
+// - Added each value from the object into a cell
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
